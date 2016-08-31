b3bc666ed0b9b78487bcf340d13694ab6c7bfcb3
DaniAkash <eaglearrow.arrow@gmail.com>
2016-09-01 01:04:41 +0530

    Serving static HTML,CSS and JS files

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ var express = require('express');
 var app = express();
 var port = 3000;
 
+/**
+ * Serve the static files
+ */
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
 app.get('/', function(req, res) {
   res.send('Hello World!!');
 });
