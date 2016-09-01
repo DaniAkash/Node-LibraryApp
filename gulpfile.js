0ca37306b2c79f32ceb94faa43f55f641b0014a9
DaniAkash <eaglearrow.arrow@gmail.com>
2016-09-01 09:46:17 +0530

    Bower for Front end package management & gulp inject

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,10 @@ var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
 
 var config = {
-    jsFiles: ['*.js', 'src/**/*.js']
+    jsFiles: ['*.js', 'src/**/*.js'],
+    htmlFiles: './src/views/*.html',
+    destHtml: './src/views',
+    bowerJsFiles: './public/lib'
 };
 
 gulp.task('style', function () {
@@ -13,4 +16,16 @@ gulp.task('style', function () {
             verbose: true
         }))
         .pipe(jscs());
+});
+
+gulp.task('inject', function () {
+    var wiredep = require('wiredep').stream;
+    var options = {
+        bowerJson: require('./bower.json'),
+        directory: config.bowerJsFiles,
+        ignorePath: '../../public'
+    };
+    return gulp.src(config.htmlFiles)
+        .pipe(wiredep(options))
+        .pipe(gulp.dest(config.destHtml));
 });
