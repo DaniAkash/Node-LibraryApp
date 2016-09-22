e987b5468503cd3b0573b2fc7c35284d2cb7e7aa
DaniAkash <eaglearrow.arrow@gmail.com>
2016-09-22 22:37:44 +0530

    https images

--- app.js
+++ app.js
@@ -25,32 +25,32 @@ var booksList = [
     {
         name: 'A Little Life',
         author: 'Hanya Yanagihara',
-        image: 'http://t3.gstatic.com/images?q=tbn:ANd9GcTE0X3r_-93u35m88WGbVNN6Pyrf_witVy36yHWuZQpe77sWFyW'
+        image: 'https://t3.gstatic.com/images?q=tbn:ANd9GcTE0X3r_-93u35m88WGbVNN6Pyrf_witVy36yHWuZQpe77sWFyW'
     },
     {
         name: 'Fates and Furies',
         author: 'Lauren Groff',
-        image: 'http://t0.gstatic.com/images?q=tbn:ANd9GcTQ7100T0UGX52szQGU4Nx3dY1Wu4uWBsz2Pmr8NVr1j6xa8EyE'
+        image: 'https://t0.gstatic.com/images?q=tbn:ANd9GcTQ7100T0UGX52szQGU4Nx3dY1Wu4uWBsz2Pmr8NVr1j6xa8EyE'
     },
     {
         name: 'The Girl on the Train',
         author: 'Paula Hawkins',
-        image: 'http://t0.gstatic.com/images?q=tbn:ANd9GcT-ACZX5Ej0uOo7Qglqf-q_PYan0_uNSSsG2UDn-zJgVpDCqx-r'
+        image: 'https://t0.gstatic.com/images?q=tbn:ANd9GcT-ACZX5Ej0uOo7Qglqf-q_PYan0_uNSSsG2UDn-zJgVpDCqx-r'
     },
     {
         name: 'Purity',
         author: 'Jonathan Franzen',
-        image: 'http://t0.gstatic.com/images?q=tbn:ANd9GcQDeEN2DGFT0M8PM-p6_Cob1IpXc6mxKan1erV3-wc8WUcPWq2k'
+        image: 'https://t0.gstatic.com/images?q=tbn:ANd9GcQDeEN2DGFT0M8PM-p6_Cob1IpXc6mxKan1erV3-wc8WUcPWq2k'
     },
     {
         name: 'Go Set a Watchman',
         author: 'Harper Lee',
-        image: 'http://t1.gstatic.com/images?q=tbn:ANd9GcSmNGmse2TePp9ybkrbyKW3BkdWoe1DW_s0hfNLKWThhjlCGtPu'
+        image: 'https://t1.gstatic.com/images?q=tbn:ANd9GcSmNGmse2TePp9ybkrbyKW3BkdWoe1DW_s0hfNLKWThhjlCGtPu'
     },
     {
         name: 'The Buried Giant',
         author: 'Kazuo Ishiguro',
-        image: 'http://t2.gstatic.com/images?q=tbn:ANd9GcQL_u5u9SBo1qjasx3cWVoATF0pVYbvLmED3vAFL9mNmUuAyven'
+        image: 'https://t2.gstatic.com/images?q=tbn:ANd9GcQL_u5u9SBo1qjasx3cWVoATF0pVYbvLmED3vAFL9mNmUuAyven'
     }
 ];
 
